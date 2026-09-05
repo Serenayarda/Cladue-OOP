5b33947f2af2b512a5fada0b60ce4e45e0c4b44d
serenayarda <serenayy4677@gmail.com>
2026-09-05 15:23:15 +0300

    htmlde sayfalar arası bağlantı ve resim ekleme

--- javascript/insan.js
+++ javascript/insan.js
@@ -19,10 +19,20 @@ console.log("insan uyuyor");
 } 
 } // bitiş scope
 
-//var = nesneadi  = new ClassAdi();  // nesne oluşturma
+yasHesapla(dogumyili) 
+{ var bulundugumuzyil = new date().getFullYear();
+  var sonuc = bulundugumuzyil - dogumyili;
+  return sonuc;
+}  
+
+
+  //var = nesneadi  = new ClassAdi();  // nesne oluşturma
 var i = new Insan()
 i.adi = "hakan ";
 i.soyadi = "yılmaz";
 i.yas = 25;
 i.cinsiyet = true;
 i.uyu();
+i.yasHesapla(1998);
+vargelenyas = i.yasHesapla(1998);
+console.log("gelen yas: " + gelenyas);  
